858d721dc58bc9afa4d96bf36f87013f3a79e4bc
ZarifulinR <fred-c112@yandex.ru>
2025-12-16 20:30:49 +0300

    Cw DONE

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,37 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
+    User Ivan = new User("Ivan",23);
+    User Fedor = new User("Fedor",23);
+    System.out.println(Ivan.equals(Fedor));
+    System.out.println(Ivan.toString());
+    System.out.println(Fedor.toString());
 
-    List<Playable>  player = new ArrayList<>();
-    player.add(new MusicPlayer());
-    player.add(new VideoPlaye());
-    for(Playable i: player){
-        i.play();
+    try {
+        Product TV = new Product("TV", 100);
+        System.out.println(TV);
+        Product PS= new Product("PS", -5);
+        System.out.println(PS);
     }
-    Validator email = new EmailValidator();
-    email.isValid("@.");
-    Validator pass = new PasswordValidator();
-    pass.isValid("s");
+    catch (Exception e) {
+        System.out.println(e.getMessage());
+    }
+    List<OrderItem> chek = new ArrayList<>();
+    chek.add(new OrderItem("Banana", 2, 150));
+    chek.add(new OrderItem("Aple", 4, 250));
+    chek.add(new OrderItem("Orange", 5, 500));
+
+    double totalSum = chek.stream()
+            .mapToDouble(OrderItem::totalPrice)
+            .sum();
+
+    System.out.println("Общая стоимость: " + totalSum);
+
+    OrderItem maxItem = chek.stream()
+            .max(Comparator.comparingDouble(OrderItem::totalPrice))
+            .orElse(null);
+
+    System.out.println(maxItem);
+
+
 }
